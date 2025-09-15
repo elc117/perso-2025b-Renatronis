847a9ad5f13dbf85e59c5db06ab68b649cc9d7c6
Renatronis <renato.marquioro05@gmail.com>
2025-09-15 19:52:37 -0300

    Tipos de dados definidos

--- src/Tipos.hs
+++ src/Tipos.hs
@@ -1,2 +1,20 @@
 module Tipos where
 
+data Questao = Questao {
+    id :: Int,
+    texto :: String,
+    alternativas :: [String],
+    resposta_certa :: Int
+} deriving (Show, Eq)
+
+data Resposta = Resposta {
+    questao_respondida :: Int,
+    alternativa_escolhida :: Int
+} deriving (Show, Eq)
+
+data Resultado = Resultado {
+    saber_resposta_correta :: Bool,
+    guardar_resposta_certa :: Int,
+    alt_escolhida :: Int
+} deriving (Show, Eq)
+     
